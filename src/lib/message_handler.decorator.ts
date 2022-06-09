7c32664a0ff624622ff980493293823fb384cb25
antonSvetin <anton@global.id>
2022-06-09 16:38:04 +0200

    chore(params): Removing auto params functinality

--- src/lib/message_handler.decorator.ts
+++ src/lib/message_handler.decorator.ts
@@ -4,7 +4,6 @@ import * as helper from './helper'
 import * as message from './message'
 import * as storage from './storage'
 import * as interfaces from './message_handler.interfaces'
-import * as paramDecorators from './message_handler.param.decorator'
 import * as validationI from './validation.interfaces'
 import * as decoratorUtils from './message_handler.decorator_utils'
 /**
@@ -16,7 +15,7 @@ export const MessageHandler =
     const targetName = helper.getTargetConstructor(target).name
     const isStaticMethod = target instanceof Function
     const targetConstructor = isStaticMethod ? target : target.constructor
-    const { targetMessage, methodParams } = manageAutoDecoratedArguments(target, propertyKey)
+    const targetMessage = manageAutoDecoratedArguments(target, propertyKey)
 
     if (replyClass !== undefined && !message.isMessageDecoratedClass(replyClass)) {
       throw new validationI.ValidationError('INVALID_HANDLER_REPLY_CLASS', 'MessageHandler() replyClass should be class decorated with @Message()', {
@@ -35,8 +34,7 @@ export const MessageHandler =
       replyMessageClass: replyClass,
       messageClass: targetMessage,
       origDecoratorConfig: config,
-      callback: <(...args: unknown[]) => Promise<unknown>>handlerCallback(methodParams, <(...args: unknown[]) => Promise<unknown>>descriptor.value),
-      // callback: <(...args: unknown[]) => Promise<unknown>>descriptor.value,
+      callback: <(...args: unknown[]) => Promise<unknown>>descriptor.value,
     }
 
     // a class can have multiple @MessageHandler() methods
@@ -49,41 +47,15 @@ export const MessageHandler =
     storage.SetMetadata<string, Object[]>(storage.IRIS_MESSAGE_HANDLERS, msgHandlers)(targetConstructor)
   }
 
-function manageAutoDecoratedArguments(target: Object, propertyKey: string | symbol): interfaces.MessageHandlerOptions<unknown> {
+function manageAutoDecoratedArguments(target: Object, propertyKey: string | symbol): Object {
   const methodArgs = <typeof Function[]>Reflect.getMetadata('design:paramtypes', target, propertyKey)
-  let targetMessage: Object | undefined
-  const methodParams: interfaces.MessageHandlerParamI<unknown>[] = methodArgs.map((param, index) => {
-    let handle = (msg: paramDecorators.AmqpMessage): unknown => msg
-    if (message.isMessageDecoratedClass(param)) {
-      targetMessage = param
-      const msgMeta = message.getMessageDecoratedClass(targetMessage)
-      handle = paramDecorators.MessageParam(msgMeta)
-    } else if (paramDecorators.isAmqpMessageClass(param)) {
-      handle = paramDecorators.AmqpMessageParam()
-    }
+  const targetMessage: Object[] = methodArgs.filter(message.isMessageDecoratedClass)
 
-    return {
-      parameterIndex: index,
-      handle,
-    }
-  })
-
-  if (targetMessage === undefined) {
+  if (targetMessage.length === 0) {
     throwTargetMsgError(target, propertyKey)
   }
 
-  return { targetMessage, methodParams }
-}
-
-function handlerCallback(
-  paramsMeta: interfaces.MessageHandlerParamI<unknown>[],
-  originalMethod: (...args: unknown[]) => Promise<unknown>
-): interfaces.handlerCallbackI {
-  return async (msg: unknown) => {
-    const params = await Promise.all(paramsMeta.sort((param1, param2) => param1.parameterIndex - param2.parameterIndex).map(param => param.handle(msg)))
-
-    return originalMethod(...params)
-  }
+  return targetMessage[0]
 }
 
 function throwTargetMsgError(target: Object, propertyKey: string | symbol): never {
--- src/lib/message_handler.param.decorator.ts
+++ src/lib/message_handler.param.decorator.ts
@@ -1,43 +1,5 @@
 import * as amqplib from 'amqplib'
-import * as validation from './validation'
-import flags from './flags'
-import * as messageI from './message.interfaces'
-// import { CustomIntegration } from '../config'
 import { AMQP_MESSAGE_CLASS, SetMetadata } from './storage'
-
-// /**
-//  * Use to get a @Message() decorated class representing the event msg
-//  */
-// export const MessageParam = CustomIntegration.createParamDecorator<messageI.MessageMetadataI, unknown, unknown>(async (msgMeta, ctx) => {
-//   const event = await validation.validationClass.convertBufferToTargetClass(
-//     CustomIntegration.getAmqpMessage(ctx),
-//     msgMeta,
-//     flags.DISABLE_MESSAGE_CONSUME_VALIDATION
-//   )
-
-//   return event
-// })
-export const MessageParam = (msgMeta: messageI.MessageMetadataI) => {
-  return async (amqpMessage: AmqpMessage): Promise<unknown> => {
-    const event = await validation.validationClass.convertBufferToTargetClass(amqpMessage, msgMeta, flags.DISABLE_MESSAGE_CONSUME_VALIDATION)
-
-    return event
-  }
-}
-
-// /**
-//  * Use to get a full amqplib.Message object
-//  */
-// export const AmqpMessageParam = CustomIntegration.createParamDecorator<void, unknown, amqplib.ConsumeMessage>((_data, ctx) => {
-//   return CustomIntegration.getAmqpMessage(ctx)
-// })
-
-/**
- * Use to get a full amqplib.Message object
- */
-export const AmqpMessageParam = () => {
-  return (amqpMessage: AmqpMessage): AmqpMessage => amqpMessage
-}
 
 export class AmqpMessage implements amqplib.ConsumeMessage {
   content!: Buffer
